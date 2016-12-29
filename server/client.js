04eb9ae0f611e006e35fba7dc59521d262bbdedf
施秉志 <b03902039@ntu.edu.tw>
2016-12-29 16:25:31 +0800

    mod:client.js testing

--- server/client.js
+++ server/client.js
@@ -9,5 +9,19 @@ var socket = io.connect('http://127.0.0.1:8000');
 socket.emit('login', "aaa", "bbb");
 
 socket.on('loginAck',function(){
-        console.log("ack success!");
+        console.log("login: ack success!");
+});
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that  
+    // with toString() and then trim() 
+    var arr = d.toString().trim().split(":");
+    socket.emit('register', arr[0], arr[1]);
+  });
+
+socket.on('registerAck',function(){
+        console.log("register: ack success!");
 });
--- server/server.js
+++ server/server.js
@@ -11,20 +11,20 @@ var sequence = 0;
 
 console.log("Server start!");
 
-var userArray = fs.readFileSync('user.cfg').toString().split("\n");
-
 io.on('connection', function(socket) {
 
 	conn_client++;
 	var my_client_num = conn_client;
   	clients[my_client_num] = socket;
 
+  	/* server's handler for "login" */
 	socket.on('login',function(id, pwd){
 		console.log(id);
 		console.log(pwd);
 		/* find valid user */
 		var valid = false;
-		
+		var userArray = fs.readFileSync('user.cfg').toString().split("\n");
+
 		for(i in userArray) {
 			if(userArray[i] == id+":"+pwd){
 				console.log("VALID!");
@@ -37,6 +37,28 @@ io.on('connection', function(socket) {
 		else
 			clients[my_client_num].emit('loginAck', "fail");
 	
+	});
+	/* server's handler for "register" */
+	socket.on('register',function(id, pwd){
+		console.log(id);
+		console.log(pwd);
+
+		var valid = true;
+		var userArray = fs.readFileSync('user.cfg').toString().split("\n");
+		for(i in userArray) {
+			var arr = userArray[i].toString().split(":");
+			if(arr[0] == id){
+				console.log("USED!");
+				valid = false;
+			}
+		}
+
+		if(valid) {
+			fs.appendFile('user.cfg', id+":"+pwd+"\n", function (err){});
+			clients[my_client_num].emit('registerAck', "success");
+		} else
+			clients[my_client_num].emit('registerAck', "fail");
+
 	});
 
 	/*socket.on('disconnect',function(){
