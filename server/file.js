e92a8304ae9981767999e915f24e87d4503a9703
os <os@os-VirtualBox.(none)>
2017-01-13 11:21:42 +0800

    file.js: download

--- server/file.js
+++ server/file.js
@@ -5,18 +5,41 @@ var args = process.argv.slice(2);
 
 var socket = io.connect('http://127.0.0.1:8000');
 
-/*args[0] = sender name, args[1] = receiver name, args[2] = file name*/
-console.log(args[0]+' '+args[1]+' '+args[2]);
+/*args[0] = mode[down|up], args[1] = sender/receiver name, args[2] = sender/receiver name, args[3] = file name*/
+console.log(args[0]+' '+args[1]+' '+args[2]+' '+args[3]);
 
-fs.readFile(args[2], 'binary', function(err, data) {
-    console.log("enter");
-    if (err) {
-        console.log("cannot open file:"+args[2]);
+/*args[0] = mode:up, args[1] = sender name, args[2] = receiver name, args[3] = file name*/
+if (args[0] === "up") {
+    fs.readFile(args[3], 'binary', function(err, data) {
+        console.log("enter");
+        if (err) {
+            console.log("cannot open file:"+args[3]);
+        }
+        else {
+            console.log(path.basename(args[3]));
+            socket.emit('fileUpload', args[1], args[2], path.basename(args[3]), data);
+        }   
+    });
+}
+/*args[0] = mode:down, args[1] = receiver name, args[2] = sender name, args[3] = file name*/
+else if (args[0] === "down") {
+    console.log("download start");
+    socket.emit('fileDownload', args[1], args[2], args[3]);
+}
+else{
+    console.log("invalid command");
+    process.exit();
+}
+
+socket.on('fileDownloadAck', function(filename, data) {
+    if (filename === "") {
+        console.log("cannot fetch the file");
     }
     else {
-        console.log(path.basename(args[2]));
-        socket.emit('fileUpload', args[0], args[1], path.basename(args[2]), data);
+        fs.writeFileSync(filename, data, 'binary');
+        console.log("get "+filename+" success");
     }
+    process.exit();
 });
 
 socket.on('uploadStatus', function(stat) {
@@ -28,3 +51,4 @@ socket.on('uploadStatus', function(stat) {
     }
     process.exit();
 });
+
